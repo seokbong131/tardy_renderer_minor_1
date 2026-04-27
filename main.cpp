137b4c9fac0a71ddd54f7d68a07dfcf3c2b575e3
seokbong131 <seokbong131@gmail.com>
2026-04-28 02:27:46 +0900

    feat: integrate scene and camera system

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <format>
+#include <iostream>
 
 #include "config.h"
 #include "obj_loader.h"
+#include "scene/camera.hpp"
+#include "scene/camera_presets.hpp"
+#include "scene/scene.hpp"
 #include "tgaimage.h"
 
 int main(int argc, char** argv) {
@@ -24,6 +28,17 @@ int main(int argc, char** argv) {
     }
     Mesh model(meshes);*/
     // ------------------------------------------------------------
+
+    Scene scene;
+    scene.objects.push_back(SceneObject{&model});
+
+    Camera camera       = setup_default_camera(); // or set_custom_camera()
+    camera.aspect_ratio = static_cast<float>(WIDTH) / static_cast<float>(HEIGHT);
+    set_orbit(
+        camera, /*center=*/{0.0f, 0.0f, 0.0f}, /*distance=*/3.0f, /*height=*/1.0f, /*theta=*/0.0f);
+
+    std::cout << "\n[INFO] view matrix:\n" << view_matrix(camera);
+    std::cout << "\n[INFO] projection matrix:\n" << projection_matrix(camera);
 
     return 0;
 }
