83bfc6100a315e1728401999de8f125555d316f0
seokbong131 <seokbong131@gmail.com>
2026-04-28 01:54:18 +0900

    feat: add numerical constants
    C++20 <numbers>
    inline constexpr float -> One Definition Rule

---
+++ include/graphics_mathematics/constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <numbers>
+
+inline constexpr float PI        = std::numbers::pi_v<float>;
+inline constexpr float TWO_PI    = 2.0f * PI;
+inline constexpr float HALF_PI   = 0.5f * PI;
+inline constexpr float TO_RADIAN = PI / 180.0f;
+inline constexpr float TO_DEGREE = 180.0f / PI;
